2ba02bd529d4a6a798a93849f3f065f4187a31a0
zwanter <roritet14@gmail.com>
2024-03-23 00:00:57 +0500

    Version 0.2

--- src/main/java/ru/zwanter/utils/bukkit/items/json/GsonItemStack.java
+++ src/main/java/ru/zwanter/utils/bukkit/items/json/GsonItemStack.java
@@ -1,11 +1,16 @@
 package ru.zwanter.utils.bukkit.items.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.*;
 import com.google.gson.stream.JsonReader;
 import lombok.Getter;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.inventory.ItemStack;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class GsonItemStack {
 
@@ -17,5 +22,45 @@ public class GsonItemStack {
                 .registerTypeHierarchyAdapter(ConfigurationSerializable .class, new ConfigurationSerializableAdapter())
                 .registerTypeHierarchyAdapter(ItemStack .class, new ItemStackAdapter())
                 .create();
+
+    public static JsonElement itemToJson(ItemStack itemStack) {
+        return gson.toJsonTree(itemStack, ItemStack.class);
+    }
+
+    public static ItemStack jsonToItem(JsonElement jsonElement) {
+        return gson.fromJson(jsonElement, ItemStack.class);
+    }
+
+    public static JsonObject itemsToJson(List<ItemStack> itemStacks) {
+        JsonObject mainObject = new JsonObject();
+
+        JsonArray jsonArray = new JsonArray();
+        for (ItemStack itemStack : itemStacks) {
+            jsonArray.add(gson.toJsonTree(itemStack, ItemStack.class));
+        }
+
+        mainObject.add("items", jsonArray);
+
+        return mainObject;
+    }
+
+    public static List<ItemStack> itemsFromJson(JsonObject jsonObject) {
+        JsonArray jsonArray = jsonObject.getAsJsonArray("items");
+
+        if (jsonArray == null) {
+            throw new IllegalArgumentException("JsonArray 'items' not found in JsonObject");
+        }
+
+        List<ItemStack> itemStacks = new ArrayList<>();
+
+        for (JsonElement jsonElement : jsonArray) {
+            if (jsonElement != null) {
+                ItemStack itemStack = gson.fromJson(jsonElement, ItemStack.class);
+                itemStacks.add(itemStack);
+            }
+        }
+
+        return itemStacks;
+    }
 
 }
